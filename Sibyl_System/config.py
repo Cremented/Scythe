e39a98ef31f92034eb74fee913783524bd86346c
Cremented <110434131+Cremented@users.noreply.github.com>
2022-08-01 20:01:02 -0700

    Update config.py

--- Sibyl_System/config.py
+++ Sibyl_System/config.py
@@ -3,9 +3,9 @@ from typing import Union, Optional
 API_ID: int = 17638504
 API_HASH: str = "d96f7384bacb035e1f56a01cef59e10e"
 STRING_SESSION: str = "1AZWarzYBux2uNcegcWX_tAvU9DNS0bFpP0YNyV7xD9_0HXGPuDZZVjdmeGgwBK6XxZCgBsm47Hzuw1PUHB45lv4AugxWAzCgsm1ICt8RLjoxyo8bhQflBU3ONeDTf-YQ_YAYfNcNJn3zglHSgH8D4kkl9xq4j0XHJGjzDnjbY1aJ6ztvjM1bf4HeV7vQ6yflDLmL88_8SG8uOKxEkyR1FaCreqmkYbjamro7DvwptvMmPJAk6CKH83MQ3HTLQDa1D0a-r6jxqYalbNhzThAftVc1__zu9rh_wkC7KLzxjBV6G63CG5Ezga63VPUdJcRrSm0SgC2WdeZ_oVCvWng7p1vpG8eIrXM="
-Sibyl_logs: Union[str, int] = "-1001685853316"
-Sibyl_approved_logs: Union[str, int] = "-1001690074708"
-GBAN_MSG_LOGS: Optional[Union[str, int]] = "-1001559327732"
+Sibyl_logs: Union[str, int] =-1001685853316
+Sibyl_approved_logs: Union[str, int] =-1001690074708
+GBAN_MSG_LOGS: Optional[Union[str, int]] =-1001559327732
 BOT_TOKEN: str = "5493778479:AAGt0Hbyok-UOqqyn9wFa4ORfPU8PwcCnGw"
 MONGO_DB_URL: str = "mongodb+srv://ok:lol@cluster1.udhzs7r.mongodb.net/?retryWrites=true&w=majority"
 ADMIN_API_KEY: str = "xaxaxa"
