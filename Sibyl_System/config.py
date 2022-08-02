c682a74e24d2b876c08c239d8452941cb2f0f979
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-08-02 08:11:55 +0530

    Update config.py

--- Sibyl_System/config.py
+++ Sibyl_System/config.py
@@ -1,14 +1,14 @@
 from typing import Union, Optional
 
-API_ID: int = 12345
-API_HASH: str = "XYZ"
-STRING_SESSION: str = "XYZ:XYZ:XYZ...."
-Sibyl_logs: Union[str, int] = 123456789
-Sibyl_approved_logs: Union[str, int] = 123456789
-GBAN_MSG_LOGS: Optional[Union[str, int]] = None
-BOT_TOKEN: str = "123456:shsiajskzkxhxj"
-MONGO_DB_URL: str = "Mongo Url here"
+API_ID: int = 17638504
+API_HASH: str = "d96f7384bacb035e1f56a01cef59e10e"
+STRING_SESSION: str = "1AZWarzYBux2uNcegcWX_tAvU9DNS0bFpP0YNyV7xD9_0HXGPuDZZVjdmeGgwBK6XxZCgBsm47Hzuw1PUHB45lv4AugxWAzCgsm1ICt8RLjoxyo8bhQflBU3ONeDTf-YQ_YAYfNcNJn3zglHSgH8D4kkl9xq4j0XHJGjzDnjbY1aJ6ztvjM1bf4HeV7vQ6yflDLmL88_8SG8uOKxEkyR1FaCreqmkYbjamro7DvwptvMmPJAk6CKH83MQ3HTLQDa1D0a-r6jxqYalbNhzThAftVc1__zu9rh_wkC7KLzxjBV6G63CG5Ezga63VPUdJcRrSm0SgC2WdeZ_oVCvWng7p1vpG8eIrXM="
+Sibyl_logs: Union[str, int] = -1001515730738
+Sibyl_approved_logs: Union[str, int] = -1001690074708
+GBAN_MSG_LOGS: Optional[Union[str, int]] = -1001559327732
+BOT_TOKEN: str = "5493778479:AAGt0Hbyok-UOqqyn9wFa4ORfPU8PwcCnGw"
+MONGO_DB_URL: str = "mongodb+srv://ok:lol@cluster1.udhzs7r.mongodb.net/?retryWrites=true&w=majority"
 ADMIN_API_KEY: str = "xaxaxa"
 ADMIN_API_HOST: str = "http://owo.com"
 
-# Remove this line one you have read and understood the explanation of things in https://github.com/AnimeKaizoku/SibylSystem/blob/master/README.md
+
